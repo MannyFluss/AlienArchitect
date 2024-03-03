fa96544e10f964220c8cf40312af0ca2fb537bb2
Manny-OnGit <manny.fluss@gmail.com>
2024-03-02 21:25:53 -0800

    yup

--- Prefabs/LevelUI/LevelUI.gd
+++ Prefabs/LevelUI/LevelUI.gd
@@ -1,11 +1,21 @@
 extends CanvasLayer
+class_name LevelUI
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+
+#this script should only be cosmetic and reactive to other changes
+
+func updateMunicipalityPoints(points:int)->void:
+	%CurrentProgressLabel.text = "Current Progress: "+str(points)
+
+func updateQuotaLabel(quotaAmount:int)->void:
+	%QuotaLabel.text = "Quota: "+str(quotaAmount) 
+
+func _on_game_state_new_municipality_score(amount: int) -> void:
+	updateMunicipalityPoints(amount)
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_game_state_quota_set(amount: int) -> void:
+	updateQuotaLabel(amount)
+	pass # Replace with function body.
--- Scripts/GameState.gd
+++ Scripts/GameState.gd
@@ -3,6 +3,37 @@ class_name GameState
 
 signal cardPlayed(_card:Card)
 
+#make all of these part of a resource eventually
+signal levelCompleted
+
+signal quotaSet(amount:int)
+signal newMunicipalityScore(amount:int)
+
+
 @export
 var bluePrintCount:int = 10
+@export
+var quota:int=0
 
+var currentMunicipalityScore:int=0
+
+
+
+
+var levelCompleteFlag := false
+
+func _ready() -> void:
+	GameStateUtility.connect("generatedScore",onGeneratedScore)
+	emit_signal("quotaSet",quota)
+	
+	
+	
+func onGeneratedScore(amount:int,building:Building)->void:
+	increaseScore(amount)
+	
+func increaseScore(amount:int)->void:
+	currentMunicipalityScore += amount
+	emit_signal("newMunicipalityScore",currentMunicipalityScore)
+	if currentMunicipalityScore >= quota && levelCompleteFlag==false:
+		levelCompleteFlag=true
+		emit_signal("levelCompleted")
