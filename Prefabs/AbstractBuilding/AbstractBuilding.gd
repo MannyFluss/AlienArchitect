188c75c570b915214d45ddf7b7b337c4415fdfaa
Manny-OnGit <manny.fluss@gmail.com>
2024-03-03 11:21:18 -0800

    working on modules

--- Prefabs/AbstractBuilding/AbstractBuilding.gd
+++ Prefabs/AbstractBuilding/AbstractBuilding.gd
@@ -2,13 +2,14 @@ extends Node3D
 class_name Building
 
 
+var modulePath : String = "res://components/BuildingModules/"
+
 @export
 var myResource : BuildingResource
 
-func _ready() -> void:
+func _enter_tree() -> void:
 	if myResource == null:
 		push_error("error myResource is null")
-	
 	if myResource.Model!= null:
 		var to_add : Node3D = myResource.Model.instantiate()
 		$Models.add_child(to_add)
@@ -18,8 +19,16 @@ func placeMe(newParent:Node3D)->void:
 	position=Vector3.ZERO
 	newParent.add_child(self)
 	
-func loadModule(module : BuildingModuleResource)->void:
+func loadModulePackage(moduleContainer : BuildingModuleContainer)->void:
+	for module : BuildingModuleResource in moduleContainer.buildingModules:
+		loadModule(module)
 	
-	pass
+	
+func loadModule(module : BuildingModuleResource)->void:
+	var moduleName :String = module.moduleName
+	var options :Dictionary = module.options
+	var path : String = modulePath+moduleName+"/"+moduleName+".tscn"
+	if FileAccess.file_exists(path)==false:return
+		
 	
 
