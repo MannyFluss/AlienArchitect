bd90066dcaf0033276018eec2532281725192339
Manny-OnGit <manny.fluss@gmail.com>
2024-04-04 23:09:01 -0700

    update EventQueue

--- Scripts/CustomNodes/EventQueue.gd
+++ Scripts/CustomNodes/EventQueue.gd
@@ -12,20 +12,25 @@ var active : bool = false
 var debugEnabled : bool = false
 
 func _ready()->void:
-	var newEvent : QueueEvent = QueueEvent.new(testReference,testReference.testingFunction,testReference.timeout)
-	var newEvent2 : QueueEvent = QueueEvent.new(testReference,testReference.testingFunction,testReference.timeout)
-	var newEvent3 : QueueEvent = QueueEvent.new(testReference,testReference.testingFunction,testReference.timeout)
-	
-	myEventQueue.append(newEvent)
-	myEventQueue.append(newEvent2)
-	myEventQueue.append(newEvent3)
-	
-	executeQueue()
-	
-	
-func callmeback()->void:
-	print("called")
+	pass
+	#var newEvent : QueueEvent = QueueEvent.new(testReference,testReference.testingFunction,testReference.timeout)
+	#var newEvent2 : QueueEvent = QueueEvent.new(testReference,testReference.testingFunction,testReference.timeout)
+	#var newEvent3 : QueueEvent = QueueEvent.new(testReference,testReference.testingFunction,testReference.timeout)
+	#
+	#myEventQueue.append(newEvent)
+	#myEventQueue.append(newEvent2)
+	#myEventQueue.append(newEvent3)
+	#
+	#executeQueue()
+	#
 
+func pushEventFront(_event : QueueEvent)->void:
+	if myEventQueue.is_empty():
+		myEventQueue.push_front(_event)
+		active=true
+		executeQueue()
+	else:
+		myEventQueue.push_front(_event)
 func executeQueue()->void:
 	if active==true:
 		push_error("queue is already active")
